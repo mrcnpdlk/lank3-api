b0f35c729e6f9bf2ab2fc30951a90ec43038814e
Marcin Pudełek <mrcnpdlk@gmail.com>
2018-11-06 10:25:41 +0100

    first commit

---
+++ src/Oid.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 05.11.2018
+ * Time: 23:15
+ */
+
+namespace mrcnpdlk\Lib\Lank3;
+
+
+class Oid
+{
+    public const OID_OUT0     = 'iso.3.6.1.4.1.7616.3.1.0.0';
+    public const OID_OUT1     = 'iso.3.6.1.4.1.7616.3.1.1.0';
+    public const OID_OUT2     = 'iso.3.6.1.4.1.7616.3.1.2.0';
+    public const OID_OUT3     = 'iso.3.6.1.4.1.7616.3.1.3.0';
+    public const OID_OUT4     = 'iso.3.6.1.4.1.7616.3.1.4.0';
+    public const OID_OUT5     = 'iso.3.6.1.4.1.7616.3.1.5.0';
+    public const OID_PWM0     = 'iso.3.6.1.4.1.7616.3.1.6.0';
+    public const OID_PWM1     = 'iso.3.6.1.4.1.7616.3.1.7.0';
+    public const OID_PWM2     = 'iso.3.6.1.4.1.7616.3.1.8.0';
+    public const OID_PWM3     = 'iso.3.6.1.4.1.7616.3.1.9.0';
+    public const OID_INPD1    = 'iso.3.6.1.4.1.7616.3.2.1.0';
+    public const OID_INPD2    = 'iso.3.6.1.4.1.7616.3.2.2.0';
+    public const OID_INPD3    = 'iso.3.6.1.4.1.7616.3.2.3.0';
+    public const OID_INPD4    = 'iso.3.6.1.4.1.7616.3.2.4.0';
+    public const OID_INPA1    = 'iso.3.6.1.4.1.7616.3.3.1.0';
+    public const OID_INPA2    = 'iso.3.6.1.4.1.7616.3.3.2.0';
+    public const OID_INPA3    = 'iso.3.6.1.4.1.7616.3.3.3.0';
+    public const OID_INPA4    = 'iso.3.6.1.4.1.7616.3.3.4.0';
+    public const OID_INPA5    = 'iso.3.6.1.4.1.7616.3.3.5.0';
+    public const OID_INPA6    = 'iso.3.6.1.4.1.7616.3.3.6.0';
+    public const OID_VIN      = 'iso.3.6.1.4.1.7616.3.3.7.0';
+    public const OID_TEMP     = 'iso.3.6.1.4.1.7616.3.3.8.0';
+    public const OID_DS1      = 'iso.3.6.1.4.1.7616.3.4.1.0';
+    public const OID_DS2      = 'iso.3.6.1.4.1.7616.3.4.2.0';
+    public const OID_DS3      = 'iso.3.6.1.4.1.7616.3.4.3.0';
+    public const OID_DS4      = 'iso.3.6.1.4.1.7616.3.4.4.0';
+    public const OID_DS5      = 'iso.3.6.1.4.1.7616.3.4.5.0';
+    public const OID_DS6      = 'iso.3.6.1.4.1.7616.3.4.6.0';
+    public const OID_AM2320_T = 'iso.3.6.1.4.1.7616.3.4.7.0';
+    public const OID_AM2320_H = 'iso.3.6.1.4.1.7616.3.4.8.0';
+    public const OID_DIFF1    = 'iso.3.6.1.4.1.7616.3.4.9.0';
+    public const OID_DIFF2    = 'iso.3.6.1.4.1.7616.3.4.10.0';
+    public const OID_POWER1   = 'iso.3.6.1.4.1.7616.3.5.1.0';
+    public const OID_POWER2   = 'iso.3.6.1.4.1.7616.3.5.2.0';
+    public const OID_POWER3   = 'iso.3.6.1.4.1.7616.3.5.3.0';
+    public const OID_POWER4   = 'iso.3.6.1.4.1.7616.3.5.4.0';
+    public const OID_ENERGY1  = 'iso.3.6.1.4.1.7616.3.5.5.0';
+    public const OID_ENERGY2  = 'iso.3.6.1.4.1.7616.3.5.6.0';
+    public const OID_ENERGY3  = 'iso.3.6.1.4.1.7616.3.5.7.0';
+    public const OID_ENERGY4  = 'iso.3.6.1.4.1.7616.3.5.8.0';
+}
